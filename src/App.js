ce5b8723bb78e7c0f8f51c679fedf516ac09e2be
Luísa Kiefer <91638544+luisakiefer@users.noreply.github.com>
2022-07-06 08:42:20 -0300

    fixed css issues

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
 
-      <main>
-        <Dictionary defaultKeyword="word" />
-      </main>
+        <main>
+          <Dictionary defaultKeyword="word" />
+        </main>
 
-      <footer className="App-footer">
-        This project was coded by{" "}
-        <a
-          href="https://github.com/luisakiefer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Luísa Kiefer
-        </a>{" "}
-        and hosted by {" "} 
-        <a href="https://netlify.com" target="_blank" rel="noreferrer">Netlify</a>.
-      </footer>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a
+            href="https://github.com/luisakiefer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Luísa Kiefer
+          </a>{" "}
+          and hosted by{" "}
+          <a href="https://netlify.com" target="_blank" rel="noreferrer">
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
